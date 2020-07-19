515049fe808e6b427e7438105111662d70b55434
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2020-07-19 22:55:13 +0530

    initial design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
+import styles from "./styles.css";
+import img from "./indexgs.jpg";
 
-import "./styles.css";
-const WishItem={
-  item:"Kindle",
-  link:"https://xyz.com/kindle"
-}
 function Wishlist(){
   return(
     <>
-      <h3>{WishItem.item}</h3>
-      <a href={WishItem.link}>Buy</a>
+    <section className={styles["about-us"]}>
+      <h2>About Us</h2>
+      <div className={styles["about-us-container"]}>
+      <img src={img} alt="reading a book"></img>
+    </div>
+    </section>
     </>
-
-  
   )
 }
 export default function App() {
   return (
     <div className="App">
-      <h1>Karanth's Wishlist</h1>
-      <h2>A react demo</h2>
       <Wishlist/>
     </div>
   );
