367fa9e37547e62424247f535d8ae7717400a370
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2020-07-19 23:23:42 +0530

    Minor additions npm package react-bootstrap required.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 import styles from "./styles.css";
 import img from "./indexgs.jpg";
+import {Bootstrap,Grid,Row,Col} from "react-bootstrap";
 
 function Wishlist(){
   return(
     <>
     <section className={styles["about-us"]}>
       <h2>About Us</h2>
+      <hr></hr>
       <div className={styles["about-us-container"]}>
       <img src={img} alt="reading a book"></img>
-    </div>
+      </div>
     </section>
+    </>
+  )
+}
+function Goals(){
+  return(
+    <>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+
     </>
   )
 }
